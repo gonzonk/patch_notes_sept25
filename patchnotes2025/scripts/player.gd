7dc87f29ef9c42eceb90258e8e49fbfa348fea44
unknown <gonzalojesparzank@gmail.com>
2025-09-13 17:57:07 -0400

    cactus and bg added

---
+++ patchnotes2025/scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const speed = 16
+@onready var player: CharacterBody2D = $"."
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var x_dir := Input.get_axis('left', 'right')
+	var y_dir := Input.get_axis('up', 'down');
+	
+	player.position.x += x_dir * speed * delta
+	player.position.y += y_dir * speed * delta
+	move_and_slide()
